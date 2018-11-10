f849d8c9869260836350d86d218ab48efd63501c
xumenger <xumenger@126.com>
2018-11-10 15:41:38 +0800

    基础API

---
+++ library/nucleus/chars_in.py
@@ -0,0 +1,24 @@
+chars_in = """
+chars_in = 
+{:(s)
+    impl = 
+    {:(s, i)
+        if(equals(len(s), i),
+        {
+            None;
+        },
+        {
+            {:(which)
+                if(equals(which, "f"),
+                {
+                    char_at(i, s);
+                },
+                {
+                    impl(s, i + 1);
+                });
+            };
+        });
+    };
+    impl(s, 0);
+};
+"""
---
+++ library/nucleus/pairs.py
@@ -0,0 +1,13 @@
+pairs = """
+pair = {:(f, s)
+    {:(which)
+        if(equals(which, "f"),
+        {f;},
+        {s;});
+    };
+};
+
+first = {:(p) p("f");};
+
+second = {:(p) p("s");};
+"""
